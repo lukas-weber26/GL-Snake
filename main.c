52fbcef9a106a94a3d789f537bd97353f64f4931
Lukas <lukas.weber26@gmail.com>
2024-07-27 02:43:00 +0000

    Snake!

--- main.c
+++ main.c
@@ -30,6 +30,8 @@ void checkLink(unsigned int shaderProgram);
 void checkSuccess(unsigned int vertexShader);
 void framebuffer_size_callback(GLFWwindow * window, int width, int height);
 void processInput(GLFWwindow * window);
+void mouse_callback(GLFWwindow * window, double x_pos, double y_pos);
+void check_food_intersection();
 
 float random_float () {
     return (float) rand() / (float)(RAND_MAX/0.5);
@@ -45,42 +47,20 @@ typedef struct ball {
 	float m;
 } ball;
 
-void tension(ball * b) {
-    float k = 0.01;
-    int x_coords[2] = {-1.0, 1.0};
-    int y_coords[2] = {-1.0, 1.0};
+typedef struct snake {
+    struct snake * next_segment; 
+    float x; 
+    float y;
+    float v_x;
+    float v_y;
+    float a_x; //not sure if these will be used 
+    float a_y;
+} snake;
 
-    for (int i =0; i<2; i++) {
-	for (int j =0; j<2; j++) {
-	    
-	    float x_pos = x_coords[i];
-	    float y_pos = y_coords[j];
+snake s = {NULL, 0,0,0,0,0,0};
+ball food = {0.75,0.75,0,0,0,0,1};
 
-	    float x_dist = fabs(x_pos - b->x);
-	    float y_dist = fabs(y_pos - b->y);
-	    float absolute_force = k*(x_dist*x_dist + y_dist*y_dist);
-
-	    float x_strenght = x_dist/(x_dist+y_dist);
-	    float y_strenght = y_dist/(x_dist+y_dist);
-
-	    float x_force = x_strenght* absolute_force;
-	    float y_force = y_strenght* absolute_force;
-
-	    int mod1 = rand()%10; 
-	    int mod2 = rand()%10;
-    
-	    int delta1 = mod1 < 5 ? -1 : 1;
-	    int delta2 = mod2 < 5 ? -1 : 1;
-
-	    b->x += x_force*x_pos + ((float) delta1* ((float) (rand() % 100)))/10000;
-	    b -> y +=  y_force*y_pos  + ((float ) delta2* ((float) (rand() % 100)))/10000;
-	}
-    }
-
-}
-
-int main()
-{
+int main() {
 
     //these seem like pretty normal c library things! 
     glfwInit();
@@ -99,7 +79,8 @@ int main()
 
     //binds gl functions to the current window
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, &mouse_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwPollEvents(); //this seems to be an issue with wayland 
     
     glViewport(0, 0, 800, 800);
@@ -156,13 +137,6 @@ int main()
     glEnable(GL_MULTISAMPLE);
     
     int transform_loc = glGetUniformLocation(shaderProgram,"transform");
-    
-    ball balls[100];
-
-    for (int i = 0; i < 100; i++) {
-	ball b = {5*random_float(),5*random_float(),random_float(),random_float(),0.0,-0.05,random_float()};
-	balls[i] = b;	
-    }
 
     while(!glfwWindowShouldClose(window)) {
 	processInput(window);
@@ -172,15 +146,21 @@ int main()
 
 	glUseProgram(shaderProgram);
 	glBindVertexArray(VAO);
-	
-	for (int i = 0; i < 100; i++) {
-	    glUniform2f(transform_loc,balls[i].x, balls[i].y);
+    
+	//draw snake 
+	snake * draw_snake = &s;
+	while (draw_snake) {
+	    glUniform2f(transform_loc, draw_snake->x, draw_snake->y);
 	    glDrawArrays(GL_TRIANGLE_FAN, 0, number_of_vertices + 2);
-	    tension(&balls[i]);
+	    draw_snake = draw_snake -> next_segment;
 	}
+
+	glUniform2f(transform_loc, food.x, food.y);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, number_of_vertices + 2);
 	
 	glfwSwapBuffers(window);
 	glfwPollEvents(); //this seems to be an issue with wayland 
+	check_food_intersection();
 
     }
     
@@ -219,4 +199,115 @@ void checkLink(unsigned int shaderProgram) {
 	printf("%s",out);
 	exit(0);
     }
+}
+
+void mouse_callback(GLFWwindow * window, double x_pos, double y_pos) {
+    static float initial_x = 0;
+    static float initial_y = 0;
+
+    float current_x = (float) x_pos;
+    float current_y = (float) y_pos;
+
+    float diff_x = current_x - initial_x;
+    float diff_y = current_y - initial_y;
+
+    initial_x = current_x;
+    initial_y = current_y;
+
+    s.x -= diff_x*0.001;
+    s.y += diff_y*0.001;
+
+    //right
+    if (s.x <= -0.85) {
+        s.x = -0.85; 
+    }
+   
+    //left
+    if (s.x >= 0.95) {
+        s.x = 0.95; 
+    }
+  
+    //top
+    if (s.y <= -0.23) {
+        s.y = -0.23; 
+    }
+   
+    //bottom
+    if (s.y >= 0.95) {
+        s.y = 0.95; 
+    }
+}
+
+void check_snake_intersection() {
+    snake * temp = s.next_segment;
+
+    while (temp && temp->next_segment) {
+	float x_dist = s.x - temp->x;
+	float y_dist = s.y - temp->y;
+	float absolute_dist = sqrt(x_dist*x_dist + y_dist*y_dist);
+	if (absolute_dist < 0.03) {
+	    glfwTerminate();
+	    printf("You lost!\n");
+	    exit(0);
+	}
+
+	temp = temp->next_segment;
+    }
+}
+
+void update_position () {
+    snake * temp = &s;
+    float x_pos = temp->x;	
+    float y_pos = temp->y;
+    temp = temp -> next_segment;
+    while (temp) {
+    
+	if (temp == s.next_segment && (sqrt((temp -> x - x_pos)*(temp -> x - x_pos) + (temp -> y - y_pos)*(temp -> y - y_pos)) < 0.05)) {
+	    temp ->x += 0;
+	    temp ->y += 0;	
+	} else {
+	    temp ->x += temp ->v_x;	
+	    temp ->y += temp ->v_y;	
+	}
+
+	temp ->v_x = (x_pos - temp ->x)*0.1;
+	temp ->v_y = (y_pos - temp ->y)*0.1;
+
+
+	x_pos = temp->x;	
+	y_pos = temp->y;
+	temp = temp -> next_segment;
+    }
+
+    check_snake_intersection();
+
+}
+
+void check_food_intersection() {
+    //s, food;    
+    float dist_x = s.x - food.x;
+    float dist_y = s.y - food.y;
+    float absolute_dist = (sqrt(dist_x*dist_x + dist_y*dist_y));
+
+    if (absolute_dist <= 0.05) {
+	snake * new = malloc(sizeof(snake));
+	new ->x = food.x;
+	new ->y = food.y;
+	new ->v_x= 0;
+	new ->v_y = 0;
+	new -> next_segment = NULL;
+
+	snake * temp = &s;
+	while (temp -> next_segment) {
+	    temp = temp -> next_segment;
+	}
+	temp -> next_segment = new;
+	
+	food.x = 1.5*(random_float()-0.15); 
+	food.y = random_float();
+
+    }
+
+    update_position();
+
 }
